13a1caf9f37f2e5fb48f4484b94e307acec56a51
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2015-11-11 11:08:11 -0500

    Fix imports to io module

--- polyglotdb/io/__init__.py
+++ polyglotdb/io/__init__.py
@@ -1,15 +1,25 @@
 
 
-from .csv import (load_corpus_csv, load_feature_matrix_csv, export_corpus_csv,
+from .csv import (load_corpus_csv, load_feature_matrix_csv,
+                export_corpus_csv, inspect_csv,
                 export_feature_matrix_csv, DelimiterError)
 
-from .text_spelling import load_discourse_spelling, export_discourse_spelling
+from .text_spelling import (load_discourse_spelling,
+                        export_discourse_spelling,
+                        inspect_discourse_spelling,
+                        load_directory_spelling)
 
 from .text_transcription import (load_discourse_transcription,
                                 export_discourse_transcription,
-                                inspect_discourse_transcription)
+                                inspect_discourse_transcription,
+                                load_directory_transcription)
 
-from .text_ilg import load_discourse_ilg, export_discourse_ilg
+from .textgrid import (load_directory_textgrid,
+                        load_discourse_textgrid,
+                        inspect_discourse_textgrid)
+
+from .text_ilg import (load_discourse_ilg, export_discourse_ilg,
+                    inspect_discourse_ilg, load_directory_ilg)
 
 from .standards import (inspect_discourse_buckeye,
                             load_directory_buckeye,
