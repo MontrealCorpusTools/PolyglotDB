b17c96f93ecf227d04c163871d1278cd7783d731
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2016-05-03 17:44:26 +0100

    Neo4j not starting?

--- continuous-integration/travis/install.sh
+++ continuous-integration/travis/install.sh
@@ -23,6 +23,7 @@ if [ ! -d "$HOME/neo4j/neo4j" ]; then
   tar -xzf neo4j-community-3.0.0-unix.tar.gz -C $HOME/neo4j
   mv $HOME/neo4j/neo4j-community-3.0.0 $HOME/neo4j/neo4j
   sed -i.bak s/#dbms.security.auth_enabled=false/dbms.security.auth_enabled=false/g $HOME/neo4j/neo4j/conf/neo4j.conf
+  sed -i.bak s/dbms.directories.import=import/#dbms.directories.import=import/g $HOME/neo4j/neo4j/conf/neo4j.conf
 else
   echo "Neo4j already installed."
 fi
