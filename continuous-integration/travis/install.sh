c92e6cb4f5caa846ae66da512d846085a5cbc859
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2016-05-12 15:42:05 -0400

    Fix install script

--- continuous-integration/travis/install.sh
+++ continuous-integration/travis/install.sh
@@ -22,8 +22,8 @@ if [ ! -d "$HOME/neo4j/neo4j" ]; then
   wget http://dist.neo4j.org/neo4j-community-2.3.3-unix.tar.gz
   tar -xzf neo4j-community-2.3.3-unix.tar.gz -C $HOME/neo4j
   mv $HOME/neo4j/neo4j-community-2.3.3 $HOME/neo4j/neo4j
-  sed -i.bak s/#dbms.security.auth_enabled=false/dbms.security.auth_enabled=false/g $HOME/neo4j/neo4j/conf/neo4j.conf
-  sed -i.bak s/dbms.directories.import=import/#dbms.directories.import=import/g $HOME/neo4j/neo4j/conf/neo4j.conf
+
+  sed -i.bak s/#dbms.security.auth_enabled=false/dbms.security.auth_enabled=false/g $HOME/neo4j/neo4j/conf/neo4j-server.properties
 else
   echo "Neo4j already installed."
 fi
