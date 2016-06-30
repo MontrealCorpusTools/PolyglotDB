f946cda36e70c449538aed66ed257877396a7f19
esteng <elias.stengel@gmail.com>
2016-06-30 11:55:38 -0400

    corrected path

--- tests/conftest.py
+++ tests/conftest.py
@@ -404,7 +404,7 @@ def fave_corpus_config(graph_db, fave_test_dir):
 def summarized_config(graph_db, textgrid_test_dir):
     config = CorpusConfig('summarized', **graph_db)
     
-    acoustic_path = os.path.join(textgrid_test_dir, 'acoustic_config.TextGrid')
+    acoustic_path = os.path.join(textgrid_test_dir, 'acoustic_corpus.TextGrid')
     with CorpusContext(config) as c:
         c.reset()
         parser = inspect_textgrid(acoustic_path)
