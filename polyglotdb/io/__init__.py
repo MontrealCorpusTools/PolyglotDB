37178a90aade56bf9eedc55ddefdb40744ae91d9
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2016-08-15 08:47:13 -0400

    Update __init__.py

--- polyglotdb/io/__init__.py
+++ polyglotdb/io/__init__.py
@@ -3,11 +3,11 @@ from .helper import guess_textgrid_format
 
 from .parsers import (BuckeyeParser, CsvParser, IlgParser, OrthographyTextParser,
                     TranscriptionTextParser, TextgridParser, TimitParser,
-                    MfaParser, LabbCatParser, FaveParser)
+                    MfaParser, LabbCatParser, FaveParser, PartiturParser)
 
 from .inspect import (inspect_buckeye, inspect_csv, inspect_orthography,
                     inspect_transcription, inspect_textgrid, inspect_timit,
                     inspect_ilg, inspect_mfa, inspect_labbcat,
-                    inspect_fave)
+                    inspect_fave, inspect_partitur)
 
 from .exporters import save_results
