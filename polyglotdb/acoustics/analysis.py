c24219d35763fd7e926ea1c0939f64870b24c082
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2016-10-06 12:01:44 -0400

    Fix bug

--- polyglotdb/acoustics/analysis.py
+++ polyglotdb/acoustics/analysis.py
@@ -124,7 +124,7 @@ def analyze_pitch_short_files(corpus_context, files, call_back = None, stop_chec
             if len(genders) > 1:
                 raise(AcousticError('We cannot process files with multiple genders.'))
             i = genders.index(genders[0])
-            mappings[i].append(f.vowel_filepath)
+            mappings[i].append((f.vowel_filepath,))
     else:
         mappings.append([(os.path.expanduser(x.vowel_filepath),) for x in files])
         functions.append(generate_base_pitch_function(corpus_context, signal = False))
