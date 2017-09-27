931d28ff91821d304e544c1c18b4d111ed9a626a
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2017-09-27 14:28:07 -0400

    Versioning updates

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ class PyTest(TestCommand):
 
 if __name__ == '__main__':
     setup(name='polyglotdb',
-          version='0.0.2',
+          version='0.0.3',
           description='',
           long_description=readme(),
           classifiers=[
