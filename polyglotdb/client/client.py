b10a6f5818c5c2a3001529f1f7f3205f5b93cfea
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2018-02-15 12:17:50 -0500

    Add support for token auth

--- polyglotdb/client/client.py
+++ polyglotdb/client/client.py
@@ -8,8 +8,9 @@ from ..query.annotations import GraphQuery, DiscourseGraphQuery, SpeakerGraphQue
 
 
 class PGDBClient(object):
-    def __init__(self, host, corpus_name=None):
+    def __init__(self, host, token=None, corpus_name=None):
         self.host = host
+        self.token = token
         if self.host.endswith('/'):
             self.host = self.host[:-1]
         self.corpus_name = corpus_name
@@ -23,7 +24,7 @@ class PGDBClient(object):
 
         end_point = '/'.join([self.host, 'api', 'databases', ''])
         data = {'name': database_name}
-        resp = requests.post(end_point, data=data)
+        resp = requests.post(end_point, data=data, headers={'Authorization': 'Token {}'.format(self.token)})
         if resp.status_code not in [200, 201, 202]:
             raise ClientError('Could not create database: {}'.format(resp.text))
         return resp.json()
@@ -38,7 +39,7 @@ class PGDBClient(object):
             raise ClientError('Could not delete database, does not exist.')
 
         end_point = '/'.join([self.host, 'api', 'databases', str(database_id), ''])
-        resp = requests.delete(end_point)
+        resp = requests.delete(end_point, headers={'Authorization': 'Token {}'.format(self.token)})
         if resp.status_code != 204:
             raise ClientError('Could not delete database.')
 
@@ -52,11 +53,11 @@ class PGDBClient(object):
             else:
                 raise ClientError('Could not find database, does not exist.')
             end_point = '/'.join([self.host, 'api', 'databases', str(database_id), ''])
-            resp = requests.get(end_point)
+            resp = requests.get(end_point, headers={'Authorization': 'Token {}'.format(self.token)})
             return resp.json()
         else:
             end_point = '/'.join([self.host, 'api', 'databases', ''])
-            resp = requests.get(end_point)
+            resp = requests.get(end_point, headers={'Authorization': 'Token {}'.format(self.token)})
             return resp.json()
 
     def get_directory(self, database_name):
@@ -69,7 +70,7 @@ class PGDBClient(object):
             raise ClientError('Could not find database, does not exist.')
 
         end_point = '/'.join([self.host, 'api', 'databases', str(database_id), 'data_directory', ''])
-        resp = requests.get(end_point)
+        resp = requests.get(end_point, headers={'Authorization': 'Token {}'.format(self.token)})
         print(resp.json())
         return resp.json()
 
@@ -82,7 +83,7 @@ class PGDBClient(object):
         else:
             raise ClientError('Could not find database, does not exist.')
         end_point = '/'.join([self.host, 'api', 'databases', str(database_id), 'ports', ''])
-        resp = requests.get(end_point)
+        resp = requests.get(end_point, headers={'Authorization': 'Token {}'.format(self.token)})
         return resp.json()
 
     def corpus_status(self, corpus_name=None):
@@ -95,16 +96,16 @@ class PGDBClient(object):
             else:
                 raise ClientError('Could not find corpus, does not exist.')
             end_point = '/'.join([self.host, 'api', 'corpora', str(corpus_id), ''])
-            resp = requests.get(end_point)
+            resp = requests.get(end_point, headers={'Authorization': 'Token {}'.format(self.token)})
             return resp.json()
         else:
             end_point = '/'.join([self.host, 'api', 'corpora', ''])
-            resp = requests.get(end_point)
+            resp = requests.get(end_point, headers={'Authorization': 'Token {}'.format(self.token)})
             return resp.json()
 
     def list_databases(self):
         end_point = '/'.join([self.host, 'api', 'databases', ''])
-        resp = requests.get(end_point)
+        resp = requests.get(end_point, headers={'Authorization': 'Token {}'.format(self.token)})
         return resp.json()
 
     def list_corpora(self, database_name=None):
@@ -120,12 +121,12 @@ class PGDBClient(object):
 
         else:
             end_point = '/'.join([self.host, 'api', 'corpora', ''])
-        resp = requests.get(end_point)
+        resp = requests.get(end_point, headers={'Authorization': 'Token {}'.format(self.token)})
         return resp.json()
 
     def get_source_choices(self):
         end_point = '/'.join([self.host, 'api', 'source_directories', ''])
-        resp = requests.get(end_point)
+        resp = requests.get(end_point, headers={'Authorization': 'Token {}'.format(self.token)})
         return resp.json()
 
     def run_query(self, query, blocking=False):
@@ -142,7 +143,7 @@ class PGDBClient(object):
         data['blocking'] = blocking
         assert 'corpus_name' in data
         end_point = '/'.join([self.host, 'api', 'corpora', str(corpus_id), 'query', ''])
-        resp = requests.post(end_point, json=data)
+        resp = requests.post(end_point, json=data, headers={'Authorization': 'Token {}'.format(self.token)})
         if resp.status_code not in [200, 201, 202]:
             raise ClientError('Could not run query: {}'.format(resp.text))
         print(resp.text)
@@ -165,7 +166,7 @@ class PGDBClient(object):
             end_point = '/'.join([self.host, 'api', 'corpora', ''])
             data = {'name': name, 'source_directory': source_directory,
                     'format': format, 'database': database_id}
-            resp = requests.post(end_point,data=data)
+            resp = requests.post(end_point,data=data, headers={'Authorization': 'Token {}'.format(self.token)})
             print(resp.json())
             corpus_id = resp.json()['id']
 
@@ -184,7 +185,7 @@ class PGDBClient(object):
         else:
             raise ClientError('Could not find database, does not exist.')
         end_point = '/'.join([self.host, 'api', 'databases', str(database_id), 'start', ''])
-        resp = requests.post(end_point, data={})
+        resp = requests.post(end_point, data={}, headers={'Authorization': 'Token {}'.format(self.token)})
         print(resp)
         if resp.status_code not in [200, 201, 202]:
             raise ClientError('Could not start database: {}'.format(resp.text))
@@ -198,13 +199,13 @@ class PGDBClient(object):
         else:
             raise ClientError('Could not find database, does not exist.')
         end_point = '/'.join([self.host, 'api', 'databases',  str(database_id), 'stop', ''])
-        resp = requests.post(end_point, data={})
+        resp = requests.post(end_point, data={}, headers={'Authorization': 'Token {}'.format(self.token)})
         if resp.status_code not in [200, 201, 202]:
             raise ClientError('Could not stop database: {}'.format(resp.text))
 
     def get_current_corpus_status(self, corpus_name):
         end_point = '/'.join([self.host, 'api', 'corpus_status', corpus_name, ''])
-        resp = requests.get(end_point)
+        resp = requests.get(end_point, headers={'Authorization': 'Token {}'.format(self.token)})
         if resp.status_code not in [200, 201, 202]:
             raise ClientError('Could not get corpus status: {}'.format(resp.text))
         return resp.json()
@@ -218,7 +219,7 @@ class PGDBClient(object):
         else:
             raise ClientError('Could not find corpus, does not exist.')
         end_point = '/'.join([self.host, 'api', 'corpora', str(corpus_id), ''])
-        resp = requests.delete(end_point)
+        resp = requests.delete(end_point, headers={'Authorization': 'Token {}'.format(self.token)})
         if resp.status_code != 204:
             raise ClientError('Could not delete corpus: {}'.format(resp.text))
 
@@ -231,7 +232,7 @@ class PGDBClient(object):
         else:
             raise ClientError('Could not find corpus, does not exist.')
         end_point = '/'.join([self.host, 'api', 'corpora', str(corpus_id), 'hierarchy', ''])
-        resp = requests.get(end_point)
+        resp = requests.get(end_point, headers={'Authorization': 'Token {}'.format(self.token)})
         if resp.status_code not in [200, 201, 202]:
             raise ClientError('Could not retrieve corpus hierarchy: {}'.format(resp.text))
         h = Hierarchy()
--- polyglotdb/utils.py
+++ polyglotdb/utils.py
@@ -12,11 +12,11 @@ def get_corpora_list(config):
 
 
 @contextmanager
-def ensure_local_database_running(database_name, port=None):
+def ensure_local_database_running(database_name, port=None, token=None):
     if port is None:
         port = 8000
     host = 'http://localhost:{}'.format(port)
-    client = PGDBClient(host)
+    client = PGDBClient(host, token=token)
 
     try:
         response = client.create_database(database_name)
