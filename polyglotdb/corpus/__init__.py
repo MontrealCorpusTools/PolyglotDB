e459861ede4333bc64e00d139a722ed1aa63dea6
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2018-07-25 13:36:39 -0400

    DOCS: update imports

--- polyglotdb/corpus/__init__.py
+++ polyglotdb/corpus/__init__.py
@@ -1,10 +1,14 @@
 from .context import CorpusContext
 
+from .base import BaseContext
 from .audio import AudioContext
 from .importable import ImportContext
 from .lexical import LexicalContext
+from .phonological import PhonologicalContext
 from .pause import PauseContext
 from .utterance import UtteranceContext
 from .structured import StructuredContext
 from .syllabic import SyllabicContext
 from .spoken import SpokenContext
+from .summarized import SummarizedContext
+from .annotated import AnnotatedContext
