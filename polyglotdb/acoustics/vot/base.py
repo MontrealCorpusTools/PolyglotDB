cc19bcce9cf85c3a62878f4f139532a9fdd4b4d5
Michael Goodale <michael.goodale@mail.mcgill.ca>
2018-09-26 10:54:04 -0400

    simplified file conversion

--- polyglotdb/acoustics/vot/base.py
+++ polyglotdb/acoustics/vot/base.py
@@ -32,23 +32,16 @@ def analyze_vot(corpus_context,
         raise (Exception('Must encode utterances before VOT can be analyzed'))
     if not corpus_context.hierarchy.has_type_subset('phone', stop_label):
         raise Exception('Phones do not have a "{}" subset.'.format(stop_label))
-    segment_mapping = generate_utterance_segments(corpus_context, padding=PADDING).grouped_mapping('speaker')
+    segment_mapping = generate_utterance_segments(corpus_context, padding=PADDING, file_type="consonant").grouped_mapping('speaker')
     #TODO: for some strange reason the keys are ("utterance_id",) not just "utterance_id", probably should be fixed
-    stop_mapping = generate_segments(corpus_context, annotation_type='phone', subset='stops', padding=PADDING, file_type="vowel").grouped_mapping('utterance_id')
+    stop_mapping = generate_segments(corpus_context, annotation_type='phone', subset='stops', padding=PADDING, file_type="consonant").grouped_mapping('utterance_id')
     number_of_speakers = len(segment_mapping)
     for speaker in segment_mapping:
         vot_func = AutoVOTAnalysisFunction(autovot_binaries_path = corpus_context.config.autovot_path ,\
                 classifier_to_use= "/autovot/experiments/models/bb_jasa.classifier")
-
-
-        with tempfile.TemporaryDirectory() as tmpdirname:
-            for seg in segment_mapping[speaker]:
-                if (seg["utterance_id"],) in stop_mapping:
-                    print(stop_mapping[(seg["utterance_id"],)])
-                    seg.properties["vot_marks"] = [(x["begin"], x["end"]) for x in stop_mapping[(seg["utterance_id"],)]]
-                else:
-                    seg.properties["vot_marks"] = []
-                tmpfilename = "{}/{}".format(tmpdirname, seg.file_path)
-                convert_to_autovot_wav(seg.file_path, tmpfilename)
-                seg.file_path = tmpfilename
-            output = analyze_segments(segment_mapping[speaker], vot_func, stop_check=stop_check, multiprocessing=multiprocessing)
+        for seg in segment_mapping[speaker]:
+            if (seg["utterance_id"],) in stop_mapping:
+                seg.properties["vot_marks"] = [(x["begin"], x["end"]) for x in stop_mapping[(seg["utterance_id"],)]]
+            else:
+                seg.properties["vot_marks"] = []
+        output = analyze_segments(segment_mapping[speaker], vot_func, stop_check=stop_check, multiprocessing=multiprocessing)
