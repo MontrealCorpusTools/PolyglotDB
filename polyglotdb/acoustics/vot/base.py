5c3f2feace8e5c45ade73bbeaed612033ac20a5f
Michael Goodale <michael.goodale@mail.mcgill.ca>
2018-09-28 13:25:06 -0400

    Got autovot conch running over per file rather than per utt

--- polyglotdb/acoustics/vot/base.py
+++ polyglotdb/acoustics/vot/base.py
@@ -1,13 +1,12 @@
 import tempfile
 
 from conch import analyze_segments
-from conch.analysis.segments import SegmentMapping
+from conch.analysis.segments import SegmentMapping, FileSegment
 from conch.analysis.autovot import AutoVOTAnalysisFunction
 
 from ..segments import generate_utterance_segments, generate_segments
 from ...exceptions import SpeakerAttributeError
 from ..classes import Track, TimePoint
-from .helper import convert_to_autovot_wav
 from ..utils import PADDING
 
 
@@ -28,20 +27,17 @@ def analyze_vot(corpus_context,
     -------
 
     """
-    if not 'utterance' in corpus_context.hierarchy:
-        raise (Exception('Must encode utterances before VOT can be analyzed'))
     if not corpus_context.hierarchy.has_type_subset('phone', stop_label):
         raise Exception('Phones do not have a "{}" subset.'.format(stop_label))
-    segment_mapping = generate_utterance_segments(corpus_context, padding=PADDING, file_type="consonant").grouped_mapping('speaker')
-    #TODO: for some strange reason the keys are ("utterance_id",) not just "utterance_id", probably should be fixed
-    stop_mapping = generate_segments(corpus_context, annotation_type='phone', subset='stops', padding=PADDING, file_type="consonant").grouped_mapping('utterance_id')
-    number_of_speakers = len(segment_mapping)
-    for speaker in segment_mapping:
-        vot_func = AutoVOTAnalysisFunction(autovot_binaries_path = corpus_context.config.autovot_path ,\
-                classifier_to_use= "/autovot/experiments/models/bb_jasa.classifier")
-        for seg in segment_mapping[speaker]:
-            if (seg["utterance_id"],) in stop_mapping:
-                seg.properties["vot_marks"] = [(x["begin"], x["end"]) for x in stop_mapping[(seg["utterance_id"],)]]
-            else:
-                seg.properties["vot_marks"] = []
-        output = analyze_segments(segment_mapping[speaker], vot_func, stop_check=stop_check, multiprocessing=multiprocessing)
+    stop_mapping = generate_segments(corpus_context, annotation_type='phone', subset='stops', padding=PADDING, file_type="consonant").grouped_mapping('discourse')
+    segment_mapping = SegmentMapping()
+    vot_func = AutoVOTAnalysisFunction(autovot_binaries_path = corpus_context.config.autovot_path ,\
+            classifier_to_use= "/autovot/experiments/models/bb_jasa.classifier")
+    for discourse in corpus_context.discourses:
+        if (discourse,) in stop_mapping:
+            sf = corpus_context.discourse_sound_file(discourse)
+            seg = FileSegment(sf["consonant_file_path"], sf["speech_begin"], sf["speech_end"])
+            seg.properties["vot_marks"] = [(x["begin"], x["end"]) for x in stop_mapping[(discourse,)]]
+            segment_mapping.segments.append(seg)
+
+    output = analyze_segments(segment_mapping, vot_func, stop_check=stop_check, multiprocessing=multiprocessing)
