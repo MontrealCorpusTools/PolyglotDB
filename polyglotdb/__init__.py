86dbf6d433b63060194ac97c90dc60156906bbbf
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2019-06-20 16:46:50 -0400

    Version bump

--- polyglotdb/__init__.py
+++ polyglotdb/__init__.py
@@ -1,6 +1,6 @@
-__ver_major__ = 0
-__ver_minor__ = 1
-__ver_patch__ = 16
+__ver_major__ = 1
+__ver_minor__ = 0
+__ver_patch__ = 0
 __ver_tuple__ = (__ver_major__, __ver_minor__, __ver_patch__)
 __version__ = "%d.%d.%d" % __ver_tuple__
 
@@ -12,10 +12,10 @@ import polyglotdb.io as io
 
 import polyglotdb.corpus as corpus
 
-CorpusContext = corpus.CorpusContext
-
 import polyglotdb.exceptions as exceptions
 
 import polyglotdb.config as config
 
 CorpusConfig = config.CorpusConfig
+
+CorpusContext = corpus.CorpusContext
