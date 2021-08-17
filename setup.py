c4eb2c23de629390ac6add37069c135c8d4fbf3d
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2021-08-17 14:51:34 -0700

    Version bump

--- setup.py
+++ setup.py
@@ -23,10 +23,30 @@ class PyTest(TestCommand):
             errcode = pytest.main(self.test_args)
             sys.exit(errcode)
 
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    delim = ' = '
+    for line in read(rel_path).splitlines():
+        if line.startswith('__ver_major__'):
+            major_version = line.split(delim)[1]
+        elif line.startswith('__ver_minor__'):
+            minor_version = line.split(delim)[1]
+        elif line.startswith('__ver_patch__'):
+            patch_version = line.split(delim)[1].replace("'", '')
+            break
+    else:
+        raise RuntimeError("Unable to find version string.")
+    return "{}.{}.{}".format(major_version, minor_version, patch_version)
+
 
 if __name__ == '__main__':
     setup(name='polyglotdb',
-          version='1.1.0',
+          version=get_version("polyglotdb/__init__.py"),
           description='',
           long_description=readme(),
           long_description_content_type='text/markdown',
