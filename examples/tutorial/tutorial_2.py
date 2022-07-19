75088f5588b449879c06c36360d68d5e7047c325
michaelhaaf <michael.haaf@gmail.com>
2022-07-19 13:04:22 -0400

    update tutorial 2 documentation

--- examples/tutorial/tutorial_2.py
+++ examples/tutorial/tutorial_2.py
@@ -2,45 +2,44 @@ import os
 from polyglotdb import CorpusContext
 
 ## CHANGE THIS PATH to location of pg_tutorial corpus on your system
-corpus_root = '/mnt/e/Data/pg_tutorial'
+corpus_root = '/home/michael/Cloud/LibriSpeech-subset/'
+corpus_name = 'tutorial-subset'
 
-# Syllable enrichment
+## See LibriSpeech-subset/enrichment_data
+speaker_filename = "SPEAKERS.csv"
+stress_data_filename = "iscan_lexicon.csv"
+
 
 syllabics = ["ER0", "IH2", "EH1", "AE0", "UH1", "AY2", "AW2", "UW1", "OY2", "OY1", "AO0", "AH2", "ER1", "AW1",
              "OW0", "IY1", "IY2", "UW0", "AA1", "EY0", "AE1", "AA0", "OW1", "AW0", "AO1", "AO2", "IH0", "ER2",
              "UW2", "IY0", "AE2", "AH0", "AH1", "UH2", "EH2", "UH0", "EY1", "AY0", "AY1", "EH0", "EY2", "AA2",
              "OW2", "IH1"]
 
-with CorpusContext('pg_tutorial') as c:
+print("Syllable enrichment...")
+with CorpusContext(corpus_name) as c:
     c.encode_type_subset('phone', syllabics, 'syllabic')
     c.encode_syllables(syllabic_label='syllabic')
 
 
-# Utterance enrichment
-
+print("Utterance enrichment...")
 pause_labels = ['<SIL>']
-
-with CorpusContext('pg_tutorial') as c:
+with CorpusContext(corpus_name) as c:
     c.encode_pauses(pause_labels)
     c.encode_utterances(min_pause_length=0.15)
 
-# Speaker enrichment
-
-speaker_enrichment_path = os.path.join(corpus_root, 'enrichment_data', 'speaker_info.csv')
-
-with CorpusContext('pg_tutorial') as c:
+print("Speaker enrichment...")
+speaker_enrichment_path = os.path.join(corpus_root, 'enrichment_data', speaker_filename)
+with CorpusContext(corpus_name) as c:
     c.enrich_speakers_from_csv(speaker_enrichment_path)
 
-# Stress enrichment
+print("Stress enrichment...")
+lexicon_enrichment_path = os.path.join(corpus_root, 'enrichment_data', stress_data_filename)
 
-lexicon_enrichment_path = os.path.join(corpus_root, 'enrichment_data', 'iscan_lexicon.csv')
-
-with CorpusContext('pg_tutorial') as c:
+with CorpusContext(corpus_name) as c:
     c.enrich_lexicon_from_csv(lexicon_enrichment_path)
     c.encode_stress_from_word_property('stress_pattern')
 
-# Additional enrichment
-
-with CorpusContext('pg_tutorial') as c:
+print("Additional enrichment...")
+with CorpusContext(corpus_name) as c:
     c.encode_rate('utterance', 'syllable', 'speech_rate')
     c.encode_count('word', 'syllable', 'num_syllables')
