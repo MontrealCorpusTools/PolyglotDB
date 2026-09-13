2b823af357a6b429f69a13964adadedb6168bd9c
Massimo Lipari <massimo.lipari@mail.mcgill.ca>
2026-09-13 08:16:15 -0400

    Off-by-one error in import callback (#270)
    * Update helper.py
    
    * Update from_csv.py

--- polyglotdb/io/importer/from_csv.py
+++ polyglotdb/io/importer/from_csv.py
@@ -295,7 +295,7 @@ def import_csvs(
         if call_back is not None:
             call_back(
                 "Importing data for speaker {} of {} ({})...".format(
-                    i, len(statements), speaker_statements[0][4]
+                    i + 1, len(statements), speaker_statements[0][4]
                 )
             )
         for s in speaker_statements:
@@ -765,7 +765,7 @@ def import_utterance_csv(corpus_context, call_back=None, stop_check=None):
                 return
             if call_back is not None:
                 call_back(
-                    "Importing data for speaker {} of {} ({})...".format(i, len(speakers), s)
+                    "Importing data for speaker {} of {} ({})...".format(i + 1, len(speakers), s)
                 )
                 call_back(i)
 
